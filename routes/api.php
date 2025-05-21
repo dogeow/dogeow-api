0b0ae1cf4ce09a5013bf98cdad0f0c4db2ccc87d
doge <doge@gmail.com>
2025-05-21 22:40:27 +0800

    233

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Api\Thing\ItemController;
-use App\Http\Controllers\Api\Nav\CategoryController;
-use App\Http\Controllers\Api\WordController;
 use App\Http\Controllers\Api\SearchController;
 
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/db-search', [SearchController::class, 'dbSearch']);
+
+// 音乐相关路由
+Route::prefix('musics')->group(function () {
+    Route::get('/', [App\Http\Controllers\Api\MusicController::class, 'index']);
+});
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
@@ -15,9 +20,10 @@ Route::middleware('auth:sanctum')->group(function () {
     
     // 引入各个项目的路由文件
     require base_path('routes/api/item.php');
+    require base_path('routes/api/location.php');
+
     require base_path('routes/api/nav.php');
     require base_path('routes/api/note.php');
-    require base_path('routes/api/location.php');
     require base_path('routes/api/todo.php');
     require base_path('routes/api/game.php');
     require base_path('routes/api/cloud.php');
@@ -25,21 +31,4 @@ Route::middleware('auth:sanctum')->group(function () {
 
     // 批量上传图片
     Route::post('/upload/images', [ItemController::class, 'uploadBatchImages']);
-});
-
-// 公开路由
-Route::get('public-items', [App\Http\Controllers\Api\Thing\ItemController::class, 'index']);
-
-// 物品搜索路由 - 使用控制器方法
-Route::get('/things', [App\Http\Controllers\Api\Thing\ItemController::class, 'index']);
-
-// 直接查询数据库的路由
-Route::get('/db-search', [SearchController::class, 'dbSearch']);
-
-// 简单搜索路由 - 直接使用 LIKE 查询
-Route::get('/search', [SearchController::class, 'search']);
-
-// 音乐相关路由
-Route::prefix('musics')->group(function () {
-    Route::get('/', [App\Http\Controllers\Api\MusicController::class, 'index']);
 });
