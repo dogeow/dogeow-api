8485301cb432dc9b575cd1404684fb61227d954c
doge <doge@gmail.com>
2025-05-21 22:09:39 +0800

    233

--- routes/api/item.php
+++ routes/api/item.php
@@ -6,12 +6,12 @@ use App\Http\Controllers\Api\Thing\TagController;
 use Illuminate\Support\Facades\Route;
 
 // 物品
-Route::prefix('things')->group(function () {
+Route::prefix('things')->name('things.')->group(function () {
     Route::apiResource('items', ItemController::class);
-
+    
     // 分类
     Route::apiResource('categories', CategoryController::class);
-
+    
     // 标签
     Route::apiResource('tags', TagController::class);
 });
--- routes/api/note.php
+++ routes/api/note.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 // 笔记相关路由
-Route::prefix('notes')->group(function () {
+Route::prefix('notes')->name('notes.')->group(function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::apiResource('/', \App\Http\Controllers\Api\NoteController::class);
         Route::apiResource('tags', \App\Http\Controllers\Api\NoteTagController::class);
