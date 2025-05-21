bba9e570faf39bfedce90df7ce46c44e95aa3ec5
doge <doge@gmail.com>
2025-05-22 00:38:55 +0800

    233

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -27,6 +27,13 @@
       0 => 'Laravel\\Scout\\ScoutServiceProvider',
     ),
   ),
+  'laravel/telescope' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Telescope\\TelescopeServiceProvider',
+    ),
+  ),
   'laravel/tinker' => 
   array (
     'providers' => 
