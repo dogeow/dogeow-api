1bd6c7ab6b666e79e0b1a1c1658d42e3506ae94f
doge <doge@gmail.com>
2025-05-24 15:25:57 +0800

    Update note.php

--- routes/api/note.php
+++ routes/api/note.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 // 笔记相关路由
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('notes', \App\Http\Controllers\Api\NoteController::class);
-    Route::apiResource('notes.tags', \App\Http\Controllers\Api\NoteTagController::class);
-    Route::apiResource('notes.categories', \App\Http\Controllers\Api\NoteCategoryController::class);
+    Route::apiResource('notes/tags', \App\Http\Controllers\Api\NoteTagController::class);
+    Route::apiResource('notes/categories', \App\Http\Controllers\Api\NoteCategoryController::class);
 });
