e14ad08b7c30edeb15b74445bfcb1a31ae94a02b
doge <doge@gmail.com>
2026-02-20 21:22:13 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -21,19 +21,12 @@ return [
 
     'allowed_origins' => [
         env('FRONTEND_URL', 'http://localhost:3000'),
-        'http://127.0.0.1:3000',
-        'http://localhost:3000',
-        'http://127.0.0.1:3001',
-        'http://localhost:3001',
-        'http://127.0.0.1:8081',
-        'http://localhost:8081',
-        'http://127.0.0.1:8081',
-        'http://localhost:8081',
-        'https://dogeow.com',
-        'https://www.dogeow.com',
     ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^https://(.+\.)?dogeow\.com$#',
+        '#^http://(localhost|127\.0\.0\.1):\d+$#',
+    ],
 
     'allowed_headers' => ['*'],
 
